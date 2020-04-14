29b95cf5802f080220c7d520d09a0a8b1d3ff993
festeves <festeves86@hotmail.com>
2020-04-14 14:16:30 -0300

    versao_4_13_novos_campos

--- meuApp/admin.py
+++ meuApp/admin.py
@@ -1,7 +1,7 @@
-# from django.contrib import admin
-# from .models import List
-# from .models import List2
+from django.contrib import admin
+from .models import List
+from .models import List2
 
-# # # Register your models here.
-# admin.site.register(List)
-# admin.site.register(List2)
+# # Register your models here.
+admin.site.register(List)
+admin.site.register(List2)
